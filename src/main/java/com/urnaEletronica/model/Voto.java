1b31b4f5d9f9b04a88de9d00e4f73ae3c2a86540
Administrator <Administrator@DESKTOP-8H1J301>
2025-07-01 06:33:23 -0300

    atualização e comentando

--- src/main/java/com/urnaEletronica/model/Voto.java
+++ src/main/java/com/urnaEletronica/model/Voto.java
@@ -1,21 +1,29 @@
 package com.urnaEletronica.model;
 
-import java.time.LocalDateTime;
+//classe pra armazenar a data e a hora que foi feito o voto
+import java.time.LocalDateTime; 
 
+
+//classe que representa um voto na urna 
 public class Voto {
+    //número do candidato votado
     private String numeroVotado;
+    //horário do voto
     private LocalDateTime horario;
+    //voto em branco e nulo
+    private boolean branco;
+    private boolean nulo;
     
+    //definindo atributos do voto
+    public Voto(String numeroVotado, boolean branco, boolean nulo) {
+        this.numeroVotado = numeroVotado; //número digitado pelo eleitor
+        this.horario = LocalDateTime.now();//hora do voto
+        this.nulo = nulo;//se for nulo
+        this.branco = branco;//se for branco
+    }
     
-    public Voto(String numeroVotado) {
-        this.numeroVotado = numeroVotado;
-        this.horario = LocalDateTime.now();
-        
-    }
-    public String getNumeroVotado(){
-        return numeroVotado;
-    }
-    public LocalDateTime getHorario(){
-        return horario;
-    }
+     public String getNumeroVotado() { return numeroVotado; }//retorna o número do candidato votado
+     public LocalDateTime getHorario() { return horario; }//retorna a hora da votação
+     public boolean isNulo() { return nulo; }//retorn se foi nulo
+     public boolean isBranco() { return branco; }//retorna se foi branco
 }
