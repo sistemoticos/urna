cf27b25cc2b7657631d7dba40d6ba7d8e5c375e5
Administrator <Administrator@DESKTOP-8H1J301>
2025-07-01 06:54:03 -0300

    comentario

--- src/main/java/com/urnaEletronica/model/Candidato.java
+++ src/main/java/com/urnaEletronica/model/Candidato.java
@@ -1,11 +1,13 @@
 package com.urnaEletronica.model;
 
+//classe q representa um candidato da urna eletrônica
 public class Candidato {
-    private String numero;
-    private String nome;
-    private String partido;
-    private String foto;
+    private String numero;//número do candidato
+    private String nome;//nome do candidato
+    private String partido;//partido do candidato
+    private String foto;//foto do candidato
     
+    //definindo atributos de um candidato
 public Candidato(String numero, String nome, String partido, String foto){
     this.numero = numero; 
     this.nome = nome;
@@ -13,13 +15,13 @@ public Candidato(String numero, String nome, String partido, String foto){
     this.foto = foto;
 }
 
-public String getNumero()   { return numero; }
-public String getNome()   { return nome; }
-public String getPartido()   { return partido; }
-public String getFoto()   { return foto; }
+public String getNumero()   { return numero; }//retorna o número do candidato
+public String getNome()   { return nome; }//retorna o nome do candidato
+public String getPartido()   { return partido; }//retorna o partido do candidato
+public String getFoto()   { return foto; }//retorna o caminho da foto do candidato
 
-public void setNumero() { this.numero = numero; }
-public void setNome() { this.nome = nome; }
-public void setPartido() { this.partido = partido; }
-public void setFoto() { this.foto = foto; }
+public void setNumero(String numero) { this.numero = numero; }//atualiza o número do candidato
+public void setNome(String nome) { this.nome = nome; }//atualiza o nome do candidato
+public void setPartido(String partido) { this.partido = partido; }//atualiza o partido
+public void setFoto(String foto) { this.foto = foto; }//atualiza a foto do candidato
 }
