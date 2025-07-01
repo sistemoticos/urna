22997c33c69fc0a8ee2c600ac4fc2e09dd6fcd55
Administrator <Administrator@DESKTOP-8H1J301>
2025-07-01 19:05:37 -0300

    partidos

--- src/main/java/com/urnaEletronica/controller/Verificador.java
+++ src/main/java/com/urnaEletronica/controller/Verificador.java
@@ -19,7 +19,24 @@ public class Verificador {
         listaCandidatos.add(new Candidato("91003", "Ginástica Artística", "PEsp", "fotos/ginastica.jpg"));
         
         //partido dos ritmos musicais
+        listaCandidatos.add(new Candidato("92001", "Rock", "PMus", "fotos/rock.jpg"));
+        listaCandidatos.add(new Candidato("92002", "Sertanejo", "PMus", "fotos/sertanejo.jpg"));
+        listaCandidatos.add(new Candidato("92003", "MPB", "PMus", "fotos/mpb.jpg"));
+       
+        //partido das profissões
+        listaCandidatos.add(new Candidato("93001", "Astronauta", "PProf", "fotos/astronauta.jpg"));
+        listaCandidatos.add(new Candidato("93002", "Pintor", "PProf", "fotos/pintor.jpg"));
+        listaCandidatos.add(new Candidato("93003", "Enfermeira", "PProf", "fotos/enfermeira.jpg"));
         
+        //partido das festas populares
+        listaCandidatos.add(new Candidato("94001", "Páscoa", "PFest", "fotos/pascoa.jpg"));
+        listaCandidatos.add(new Candidato("94002", "Oktoberfest", "PFest", "fotos/oktober.jpg"));
+        listaCandidatos.add(new Candidato("94003", "Reveillon", "PFest", "fotos/reveillon.jpg"));
+        
+        //partido do folclore
+        listaCandidatos.add(new Candidato("95001", "Curupira", "PFolc", "fotos/curupira.jpg"));
+        listaCandidatos.add(new Candidato("95002", "Cabra Cabriola", "PFolc", "fotos/cabra.jpg"));
+        listaCandidatos.add(new Candidato("95003", "Cuca", "PFolc", "fotos/cuca.jpg"));
     }
     
     
