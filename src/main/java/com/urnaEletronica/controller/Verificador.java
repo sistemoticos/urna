593eeb0e0259810827fffb68514376745da94e88
Administrator <Administrator@DESKTOP-8H1J301>
2025-07-01 16:19:14 -0300

    verificador

---
+++ src/main/java/com/urnaEletronica/controller/Verificador.java
@@ -0,0 +1,19 @@
+package com.urnaEletronica.controller;
+
+import com.urnaEletronica.model.Candidato;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Verificador {
+    //lista pra armazenar os candidatos que vão estar disponiveis
+    private List<Candidato> listaCandidatos;
+    
+    //adicionando os candidatos
+    public Verificador(){
+        listaCandidatos = new ArrayList<>();
+    }
+    
+    
+    
+}
