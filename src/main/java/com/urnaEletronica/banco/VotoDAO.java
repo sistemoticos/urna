8dd46909ada46310425ab9ffcc27dc7cc517ddbd
Administrator <Administrator@DESKTOP-8H1J301>
2025-07-03 05:31:06 -0300

    banco de dados

---
+++ src/main/java/com/urnaEletronica/banco/VotoDAO.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.urnaEletronica.banco;
+
+/**
+ *
+ * @author Administrator
+ */
+public class VotoDAO {
+    
+}
