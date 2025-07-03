bda1359b40524d7cd82afb06d999e951dd045b5d
Administrator <Administrator@DESKTOP-8H1J301>
2025-07-03 05:47:36 -0300

    pom.xml

--- src/main/java/com/urnaEletronica/banco/VotoDAO.java
+++ src/main/java/com/urnaEletronica/banco/VotoDAO.java
@@ -1,13 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.urnaEletronica.banco;
 
-/**
- *
- * @author Administrator
- */
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.urnaEletronica.model.Voto;
+import org.bson.Document;
+
 public class VotoDAO {
-    
+    private MongoClient mongoClient;
+    private MongoDatabase database;
+    private MongoCollection<Document> colecaoVotos;
+
+    public VotoDAO() {
+        mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
+        database = mongoClient.getDatabase("urnaEletronica");
+        colecaoVotos = database.getCollection("votos");
+    }
+
+    public void registrarVoto(Voto voto) {
+        Document doc = new Document("numeroVotado", voto.getNumeroVotado())
+            .append("branco", voto.isBranco())
+            .append("nulo", voto.isNulo())
+            .append("horario", voto.getHorario().toString());
+        colecaoVotos.insertOne(doc);
+    }
+
+    public void close() {
+        mongoClient.close();
+    }
 }
